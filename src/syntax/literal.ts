e1efdcae816d32ae2585dadc0c3b9b014235e006
glebbash <glebbash@gmail.com>
2020-08-30 22:28:49 +0300

    Renamed const to literal

---
+++ src/syntax/literal.ts
@@ -0,0 +1,28 @@
+import { BValue } from '../engine/engine';
+import { BNumber, BString } from '../engine/prelude';
+import { Token } from '../lexer';
+import { Parser } from '../parser';
+import { Expression } from './expression';
+import { PrefixParser } from './prefix-op';
+
+export const LITERAL: PrefixParser<ConstExpr> = (
+    _parser: Parser,
+    token: Token,
+) =>
+    new ConstExpr(
+        token.type === "string"
+            ? new BString(token.value.slice(1, -1) as string)
+            : new BNumber(Number(token.value)),
+    )
+
+export class ConstExpr implements Expression {
+    constructor(private value: BValue) {}
+
+    eval() {
+        return this.value
+    }
+
+    print(): string {
+        return "" + this.value
+    }
+}
