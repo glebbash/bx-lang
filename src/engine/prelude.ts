fd50d8838c6ab382bafef60b0ee1bed629e50ede
glebbash <glebbash@gmail.com>
2020-09-01 16:21:55 +0300

    Add BObject, BArray + make them iterable

--- src/engine/prelude.ts
+++ src/engine/prelude.ts
@@ -1,3 +1,4 @@
+import { panic } from "../utils/panic"
 import { BValue, BWrapper } from "./engine"
 
 export class BBoolean extends BWrapper<boolean>("Boolean") {}
@@ -11,6 +12,43 @@ export function bool(b: boolean): BBoolean {
 
 export class BNumber extends BWrapper<number>("Number") {}
 
+export class BString extends BWrapper<string>("String") {
+    [Symbol.iterator]() {
+        return this.data[Symbol.iterator]()
+    }
+}
+
+export class BArray extends BWrapper<BValue[]>("Array") {
+    [Symbol.iterator]() {
+        return this.data[Symbol.iterator]()
+    }
+    toString() {
+        return `[${this.data.map((it) => it.toString()).join(", ")}]`
+    }
+}
+
+export class BObject extends BWrapper<Record<string, BValue>>("Object") {
+    get(prop: string): BValue {
+        return this.data[prop] ?? panic(`Prop ${prop} is not defined`)
+    }
+
+    set(prop: string, val: BValue) {
+        this.data[prop] = val
+    }
+
+    *[Symbol.iterator]() {
+        for (const [key, val] of Object.entries(this.data)) {
+            yield new BArray([new BString(key), val])
+        }
+    }
+
+    toString() {
+        return `{ ${Object.entries(this.data)
+            .map(([name, val]) => name + ": " + val)
+            .join(", ")} }`
+    }
+}
+
 export class BReturn extends BWrapper<BValue>("Return") {}
 
 export type BFunctionBody = (...args: BValue[]) => BValue
@@ -21,7 +59,21 @@ export class BFunction extends BWrapper<BFunctionBody>("Function") {
     }
 }
 
-export class BString extends BWrapper<string>("String") {}
+export class BRange extends BValue {
+    constructor(private start: number, private stop: number) {
+        super("Range")
+    }
+
+    *[Symbol.iterator]() {
+        for (let i = this.start; i <= this.stop; i++) {
+            yield new BNumber(i)
+        }
+    }
+
+    toString() {
+        return `${this.start}..${this.stop}`
+    }
+}
 
 export class BConst extends BValue {
     constructor(type: string) {
@@ -30,22 +82,6 @@ export class BConst extends BValue {
 
     toString() {
         return this.type
-    }
-}
-
-export class BRange extends BValue {
-    constructor(private start: number, private stop: number) {
-        super("Range")
-    }
-
-    *[Symbol.iterator]() {
-        for (let i = this.start; i < this.stop; i++) {
-            yield new BNumber(i)
-        }
-    }
-
-    toString() {
-        return `${this.start}..${this.stop}`
     }
 }
 
