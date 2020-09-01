f4bc843e8350846ca5ece5e01fa087b7a1ebb21a
glebbash <glebbash@gmail.com>
2020-09-01 16:19:43 +0300

    Add stream from iterable

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -3,3 +3,15 @@ export const stream = <T>(items: T[]) => {
     return (consume = true) =>
         i < items.length ? items[consume ? i++ : i] : null
 }
+
+export const streamIter = <T>(items: Iterable<T>) => {
+    const iterator = items[Symbol.iterator]()
+    let holding = false
+    let buff: IteratorResult<T> = { done: false, value: null as any }
+    return (consume = true) => {
+        const res = holding ? buff : iterator.next()
+        holding = !consume
+        if (holding) buff = res
+        return res.done ? null : res.value
+    }
+}
