23594549c74e1bc36e02a70e0cb408337e802703
glebbash <glebbash@gmail.com>
2020-09-06 16:19:25 +0300

    Add definable and valid checks for assignable

--- src/syntax/assignable.ts
+++ src/syntax/assignable.ts
@@ -3,6 +3,14 @@ import { BValue } from "../engine/engine"
 import { Expression } from "./expression"
 
 export abstract class AssignableExpr implements Expression {
+    isValid() {
+        return true
+    }
+
+    isDefinable() {
+        return true
+    }
+
     abstract eval(ctx: Context): BValue
 
     abstract assign(ctx: Context, value: BValue): void
