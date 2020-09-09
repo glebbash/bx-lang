cef1ef9d0a8526451d3628c9cdb639b75fd4462b
glebbash <glebbash@gmail.com>
2020-09-09 14:00:04 +0300

    Wrap FunExpr in NamedFunExpr

--- src/syntax/fun.ts
+++ src/syntax/fun.ts
@@ -1,10 +1,10 @@
 import { Atom, Context, Expression, ExprParser, subContext } from "../core"
 import { BValue } from "../engine/engine"
-import { BFunction, BReturn, VOID } from "../engine/prelude"
+import { BPausedExec, BFunction, BReturn, VOID } from "../engine/prelude"
 import { panic } from "../utils/panic"
 import { syntaxError } from "../utils/syntax-error"
 import { ARRAY } from "./array"
-import { blockOrExpr } from "./block"
+import { BlockExpr, blockOrExpr } from "./block"
 import { ExportableExpr } from "./export"
 import { IDENT, IdentExpr } from "./ident"
 
@@ -23,36 +23,13 @@ export const FUN: Atom<FunExpr | NamedFunExpr> = (parser: ExprParser) => {
     })
 
     const body = blockOrExpr(parser)
+    const fun = new FunExpr(params, body)
 
-    return name === null
-        ? new FunExpr(params, body)
-        : new NamedFunExpr(name, params, body)
-}
-
-function makeFunction(
-    ctx: Context,
-    params: string[],
-    body: Expression,
-    name?: string,
-) {
-    return new BFunction((...args: BValue[]) => {
-        const funCtx = subContext(ctx)
-        for (let i = 0; i < params.length; i++) {
-            if (i < args.length) {
-                funCtx.scope.define(params[i], args[i])
-            }
-        }
-        const res = body.eval(funCtx)
-        return res.is(BReturn) ? res.data : res
-    }, name)
+    return name === null ? fun : new NamedFunExpr(name, fun)
 }
 
 export class NamedFunExpr implements Expression, ExportableExpr {
-    constructor(
-        public name: string,
-        private params: string[],
-        private body: Expression,
-    ) {}
+    constructor(public name: string, private fun: FunExpr) {}
 
     export(exports: Set<string>): void {
         if (exports!.has(this.name)) {
@@ -62,23 +39,37 @@ export class NamedFunExpr implements Expression, ExportableExpr {
     }
 
     eval(ctx: Context) {
-        const fun = makeFunction(ctx, this.params, this.body, this.name)
+        const fun = this.fun.eval(ctx)
         ctx.scope.define(this.name, fun)
         return VOID
     }
 
     toString(symbol = "", indent = ""): string {
-        return `fun ${this.name}(${this.params.join(
+        return `fun ${this.name}(${this.fun.params.join(
             ", ",
-        )}) ${this.body.toString(symbol, indent)}`
+        )}) ${this.fun.body.toString(symbol, indent)}`
     }
 }
 
 export class FunExpr implements Expression {
-    constructor(private params: string[], private body: Expression) {}
+    constructor(public params: string[], public body: BlockExpr) {}
 
     eval(ctx: Context) {
-        return makeFunction(ctx, this.params, this.body)
+        return new BFunction((...args: BValue[]) => {
+            const funCtx = subContext(ctx)
+            const pLen = this.params.length
+            const aLen = args.length
+            for (let i = 0; i < pLen; i++) {
+                if (i < aLen) {
+                    funCtx.scope.define(this.params[i], args[i])
+                }
+            }
+            const res = this.body.eval(funCtx)
+            if (res.is(BPausedExec)) {
+                panic("Attempt to pause normal function")
+            }
+            return res.is(BReturn) ? res.data : res
+        })
     }
 
     toString(symbol = "", indent = ""): string {
