09558935e5c70aadd5f2596cc15719032d850847
glebbash <glebbash@gmail.com>
2020-09-09 14:00:47 +0300

    Add block pause

--- src/syntax/block.ts
+++ src/syntax/block.ts
@@ -1,11 +1,22 @@
 import { Atom, Context, Expression, ExprParser } from "../core"
 import { BValue } from "../engine/engine"
-import { BBreak, BContinue, BReturn, VOID } from "../engine/prelude"
+import {
+    BBreak,
+    BContinue,
+    BPausedExec,
+    BReturn,
+    BPause,
+    ExecState,
+    VOID,
+} from "../engine/prelude"
 import { Token, Tokens } from "../lexer"
 
-export function blockOrExpr(parser: ExprParser): Expression {
-    return isBlock(parser.next(false)) ? expectBlock(parser) : parser.parse()
+export function blockOrExpr(parser: ExprParser): BlockExpr {
+    return isBlock(parser.next(false))
+        ? expectBlock(parser)
+        : new BlockExpr([parser.parse()])
 }
+
 export function isBlock(token: Token): boolean {
     return token.type === "block_brace" || token.type === "block_indent"
 }
@@ -23,13 +34,34 @@ export const BLOCK: Atom<BlockExpr> = (parser: ExprParser, token: Token) => {
     )
 }
 
+export class BlockExecState implements ExecState {
+    constructor(
+        private ctx: Context,
+        private block: BlockExpr,
+        private line: number,
+    ) {}
+
+    resume(value?: BValue): BValue {
+        return this.block.eval(this.ctx, this.line, value)
+    }
+}
+
 export class BlockExpr implements Expression {
     constructor(private body: Expression[]) {}
 
-    eval(ctx: Context) {
-        let res: BValue = VOID
-        for (const expr of this.body) {
+    eval(ctx: Context, line = 0, res: BValue = VOID) {
+        for (const len = this.body.length; line < len; line++) {
+            const expr = this.body[line]
             res = expr.eval(ctx)
+            if (res.is(BPause)) {
+                return new BPausedExec({
+                    returned: res.data,
+                    execStack: [new BlockExecState(ctx, this, line + 1)],
+                })
+            } else if (res.is(BPausedExec)) {
+                res.data.execStack.unshift(new BlockExecState(ctx, this, line + 1))
+                return res
+            }
             if (res.is(BReturn) || res.is(BBreak) || res.is(BContinue)) {
                 return res
             }
