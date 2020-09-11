c4f2c983b52462b422b9e53f1f41802cda59949b
glebbash <glebbash@gmail.com>
2020-09-11 14:04:39 +0300

    Add support for yield in define

--- src/syntax/define.ts
+++ src/syntax/define.ts
@@ -1,5 +1,6 @@
 import { Atom, Context, Expression, ExprParser } from "../core"
-import { VOID } from "../engine/prelude"
+import { BValue } from "../engine/engine"
+import { BPausedExec, ExecState, VOID } from "../engine/prelude"
 import { panic } from "../utils/panic"
 import { AssignExpr } from "./assign"
 
@@ -13,12 +14,25 @@ export const define = (constant: boolean): Atom<DefineExpr> => (
     return new DefineExpr(expr, constant)
 }
 
+export class DefineExecState implements ExecState {
+    constructor(public ctx: Context, public defineExpr: DefineExpr) {}
+
+    resume(value?: BValue): BValue {
+        return this.defineExpr.eval(this.ctx, value)
+    }
+}
+
 export class DefineExpr implements Expression {
     constructor(private expr: AssignExpr, private constant: boolean) {}
 
-    eval(ctx: Context) {
+    eval(ctx: Context, val?: BValue) {
         const { assignable, value } = this.expr
-        assignable.define(ctx, value.eval(ctx), this.constant)
+        val = val ?? value.eval(ctx)
+        if (val.is(BPausedExec)) {
+            val.data.execStack.unshift(new DefineExecState(ctx, this))
+            return val
+        }
+        assignable.define(ctx, val, this.constant)
         return VOID
     }
 
