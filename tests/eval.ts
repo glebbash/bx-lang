8d52f5a0a1f81cd13735b8414b6678be4e64498d
glebbash <glebbash@gmail.com>
2021-03-13 11:35:50 +0200

    Change working dir

--- tests/eval.ts
+++ tests/eval.ts
@@ -1,4 +1,4 @@
 import { Blocks } from "../src/blocks"
 
-const core = new Blocks(process.cwd() + "/data")
+const core = new Blocks(process.cwd() + "/../bx-lang-haxe/scripts")
 core.evalFile("main")
